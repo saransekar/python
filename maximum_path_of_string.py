386b11a4c1803ac426f7dc9dc92051e23a4a4d90
[saransekar] <[saran@5gindia.net]>
2019-01-04 18:00:35 +0530

    remove .arithmetic.py.swp

---
+++ maximum_path_of_string.py
@@ -0,0 +1,52 @@
+a = []
+
+input_string = raw_input("Enter the input string:")
+
+string_list = input_string.split()
+
+print string_list
+
+
+input_user = int(input("Enter the number:"))
+
+if(input_user > 0 and input_user  <= len(string_list)):
+
+		output_user = input_user - 1
+
+		my_list = string_list[output_user]
+
+		print my_list 
+
+		del string_list[output_user]
+
+		print string_list
+
+
+else:
+
+	print "Please Correct input start from 1 to n..."
+
+
+for k in range(0, len(string_list)):
+
+	set_list = set(string_list[k]) & set(my_list)
+
+	another_list = list(set_list)
+
+	print another_list
+
+	print len(another_list)
+
+	a.append(another_list)
+
+ 	a.append(len(another_list))
+ 	
+max_string = max(a)
+
+string = ''
+
+string = ''.join(map(str,max_string))
+
+print "Maximum letter of string:", string
+
+
