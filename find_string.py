a2e221819f89cafe559df4ef13547fdd3c8cee4f
[saransekar] <[saran@5gindia.net]>
2019-01-07 10:49:58 +0530

    I changed full coding of find string program

--- find_string.py
+++ find_string.py
@@ -1,38 +1,51 @@
-list1 = ["saran", "bala", "raja", "venkat", "ram"]
 
-another_list = raw_input("Enter the input string:")
+a = []
 
-list2 = another_list.split()
+input_string = raw_input("Enter the input string:")
 
-list1.append(list2)
+string_list = input_string.split()
+
+print string_list
 
 
+input_user = int(input("Enter the number:"))
 
-num = int(input("Enter the number:"))
+if(input_user > 0 and input_user  <= len(string_list)):
 
-if(num > 0 and num < len(list1)):
+		output_user = input_user - 1
 
-		user_output = num - 1
+		my_list = string_list[output_user]
 
-		print list1[user_output]
+		print my_list 
+
+		del string_list[output_user]
+
+		print string_list
+
 
 else:
 
 	print "Please Correct input start from 1 to n..."
 
 
-my_list = ["saran", "bala", "raja", "venkat", "ram"]
+for k in range(0, len(string_list)):
 
-list2 = another_list.split()
+	set_list = set(string_list[k]) & set(my_list)
 
-my_list.append(list2)
+	another_list = list(set_list)
 
-print my_list
+	print another_list
 
-if num < len(my_list):
+	print len(another_list)
 
-	user_output = num - 1
+	a.append(another_list)
 
-	del my_list[user_output]
+ 	a.append(len(another_list))
+ 	
+max_string = max(a)
 
-print my_list
+string = ''
+
+string = ''.join(map(str,max_string)) 
+
+print "Maximum letter of string:", string
