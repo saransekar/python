fb51ed40150f12d0f786dd380b9d8f50b40ec270
[saransekar] <[saran@5gindia.net]>
2019-02-06 10:48:31 +0530

    updated refactored functions

--- Chuck-Api/main.py
+++ Chuck-Api/main.py
@@ -1,12 +1,10 @@
-
 import argparse
-
-import requests 
-
+import requests	
+from requests import ConnectionError
 import json
+import sys
 
 BASE_URL = 'http://api.icndb.com/'
-
 
 def create_parser():
 
@@ -18,15 +16,25 @@ def create_parser():
 
 	return args
 
-
 def make_connection(BASE_URL,LimitNumber):
+		
+	try:
+
+		ResponseData = requests.get(BASE_URL + 'jokes/' + 'random/' + str(LimitNumber))
+
+	except requests.ConnectionError as e:
+
+		sys.tracebacklimit = 0
+
+		raise(requests.ConnectionError("No Internet Connection"))		
+
+	return ResponseData 
+
+def get_data(ResponseData,LimitNumber):
 
 	ResJsonDataList = []
 
-	ResponseData = requests.get(BASE_URL + 'jokes/' + 'random/' + str(LimitNumber))	 
-
 	ResponseJson = json.loads(ResponseData.content)
-
 
 	for Number in range(LimitNumber):
 
@@ -36,7 +44,6 @@ def make_connection(BASE_URL,LimitNumber):
 
 	return ResJsonDataList
 
-
 def main():
 
 	ArgsInput = create_parser()
@@ -45,12 +52,13 @@ def main():
 
 	LimitNumber = (1,LimitNumber)[LimitNumber != None]
 
-	ResJsonDataList = make_connection(BASE_URL,LimitNumber)
+	ResponseData = make_connection(BASE_URL,LimitNumber)
+
+	ResJsonDataList = get_data(ResponseData, LimitNumber)
 
 	for joke_idx, joke_val in enumerate(ResJsonDataList):
 
-		print joke_idx + 1., joke_val
-
+		print joke_idx + 1, joke_val
 
 
 if __name__ == '__main__':
