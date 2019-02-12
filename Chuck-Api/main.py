53435bc62fbf5e74a7eb6b86bc8c13c228a64195
[saransekar] <[saran@5gindia.net]>
2019-02-12 16:46:24 +0530

    the main file run it

---
+++ Chuck-Api/main.py
@@ -0,0 +1,37 @@
+import api_method
+import argparse
+
+def create_parser():
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-l", type = int, metavar = 'limit', nargs = '?', help = "enter the number")
+
+	parser.add_argument("-word", choices = ['nerdy', 'explicit', ('nerdy,explicit')], metavar ='word', nargs = '?', help = "enter the word")
+
+	args = parser.parse_args()
+
+	return args
+
+
+
+if __name__ == '__main__':
+	
+	api = api_method.API_Request()
+
+	ArgsInput = create_parser()
+
+	LimitNumber = ArgsInput.l
+
+	Word = ArgsInput.word
+
+	LimitNumber = (1,LimitNumber)[LimitNumber != None]
+
+	ResponseData = api.get_response(api_method.BASE_URL,LimitNumber,Word)
+
+	ResJsonDataList = api.get_data(ResponseData, LimitNumber)
+
+	for joke_idx, joke_val in enumerate(ResJsonDataList):
+
+		print("{} {}".format(joke_idx + 1, joke_val))
+
