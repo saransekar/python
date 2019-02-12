4c5129c09be210fd26932d9dfc4a4212a2da9602
[saransekar] <[saran@5gindia.net]>
2019-02-12 16:01:07 +0530

    Again changed naming convention

--- Chuck-Api/test-case/test_main.py
+++ Chuck-Api/test-case/test_main.py
@@ -4,45 +4,41 @@ import requests
 
 class TestMethod(unittest.TestCase):
 
-	def test_make_connection_error(self):
+	def test_get_response_error(self):
 
-		Response = main.make_connection('http://api.icndb.com/jokes/random',3)
+		Response = main.get_response('http://api.icndb.com/jokes/random',3)
 
-		self.assertRaises(requests.ConnectionError,main.make_connection("ConnectionError"))
+		self.assertRaises(requests.ConnectionError,main.get_response("ConnectionError"))
 
+	def test_get_response_success(self):
 
-	def test_make_connection_success(self):
-
-		Response = main.make_connection('http://api.icndb.com/',5)
+		Response = main.get_response('http://api.icndb.com/',5)
 
 		ExpectedOutput = 200
 
 		self.assertEqual(Response,ExpectedOutput)
 
+	def test_get_response_failed(self):
 
-	def test_make_connection_failed(self):
-
-		Response = main.make_connection('http://api.icndb.com/',5)
+		Response = main.get_response('http://api.icndb.com/',5)
 
 		ExpectedOutput = [200]
 
 		self.assertNotEqual(Response,ExpectedOutput)
 
-
 	def test_get_data_success(self):
 
-		Response = main.make_connection('http://api.icndb.com/',5)
+		Response = main.get_response('http://api.icndb.com/',5)
 		
 		ResponseJson = main.get_data(Response,5)
 
 		ExpectedOutput = 5
 
-		self.assertEqual(ResponseJson,ExpectedOutput)
-	
+		self.assertEqual(ResponseJson,ExpectedOutput)	
 
 	def test_get_data_failed(self):
 
-		Response = main.make_connection('http://api.icndb.com/',5)
+		Response = main.get_response('http://api.icndb.com/',5)
 		
 		ResponseJson = main.get_data(Response,5)
 
